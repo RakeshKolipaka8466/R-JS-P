10a20117c054fe18fc844ae8320d24d061e4c40a
Rakesh <rakeshkolipka8466@gmail.com>
2023-12-06 23:09:09 +0530

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { Button,Summary, Input} from './components/button';
+import JSXEg, { FragmentEg, MultipleElements } from './components/jsx';
+import Home from './home';
+import Counter from './counter';
 
-function App() {
+function App(){
+  const  [heading,setHeading] = useState("this is the react");
+  const [description,setDescription]=useState("react is powerful");
+  const [theme,setTheme]=useState("light"); 
+
+  const changeHeading=()=>{
+    setHeading(`This is the new Heading ${Math.random()*100}`);
+    setDescription(`This is new Description ${Math.random()*10}`);
+  };
+const toggleTheme=()=>{
+  if(theme==="light"){
+    setTheme("dark");
+  }else{
+    setTheme("light");
+  }
+};
+
+  console.log("heading",heading);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <h2>{heading}</h2>
+      <p>{description}</p>
+      <button onClick={changeHeading}>Change Heading</button>
+      <button onClick={toggleTheme}>Toggle Theme</button>
+      <div className={`box-cont bg-${theme}`}>Hey Toggled successfully</div>
+      <Home />
+      <Counter />
+       <Button />
+      <Button />
+      <Summary />
+      <Input />
+      <JSXEg />
+      <MultipleElements />
+      <FragmentEg />  
+      
+      </div>
   );
 }
-
 export default App;
